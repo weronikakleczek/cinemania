dbede3df0cd8a3e7c8fe6a522460371afaa47009
weronikakleczek <weronika.kleczek@gmail.com>
2021-11-23 23:37:44 +0100

    initial commit

---
+++ cm-backend/src/main/kotlin/com/example/cinemania/CinemaniaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cinemania
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CinemaniaApplication
+
+fun main(args: Array<String>) {
+    runApplication<CinemaniaApplication>(*args)
+}
---
+++ cm-backend/src/test/kotlin/com/example/cinemania/CinemaniaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cinemania
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CinemaniaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
