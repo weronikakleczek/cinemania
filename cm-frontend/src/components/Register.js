7505875c15b7e25640a41b2fb226219753f9b8ed
weronikakleczek <weronika.kleczek@gmail.com>
2021-12-15 20:59:08 +0100

    add register to navbar

---
+++ cm-frontend/src/components/Register.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Register = () => {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Logged as:', login);
+    }
+
+    return (
+        <div className="login">
+            <h1>Zaloguj</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Email"
+                    required
+                    value={login}
+                    onChange={(e) => setLogin(e.target.value)}
+                />
+                <input 
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
+                <button>Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
