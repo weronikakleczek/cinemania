063c72ef8a173a1c3ad7fa1bc8bf7a36badc22b6
weronikakleczek <weronika.kleczek@gmail.com>
2021-12-15 20:32:57 +0100

    fix search ui

--- cm-frontend/src/components/Navbar.js
+++ cm-frontend/src/components/Navbar.js
@@ -20,7 +20,11 @@ const Navbar = () => {
                     <Link to="/search"><li className="white">Wyszukaj</li></Link>
                     <Link to="/top"><li className="white">Top 100</li></Link>
                     <Link to="/register"><li className="yellow">Zarejestruj</li></Link>
+<<<<<<< HEAD
                     <Link to="/login"><li className="beige">{ user ? user : 'Zaloguj'}</li></Link>
+=======
+                    <Link to="/login"><li className="beige">Zaloguj</li></Link>
+>>>>>>> 3ce22fa (fix search ui)
                 </ul>
             </nav>
         </div>
