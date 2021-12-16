f48e1a219131820d6114074a313ee69b69d29192
weronikakleczek <weronika.kleczek@gmail.com>
2021-12-16 15:55:03 +0100

    add register page

--- cm-frontend/src/components/Register.js
+++ cm-frontend/src/components/Register.js
@@ -2,33 +2,59 @@ import { useState } from "react";
 
 const Register = () => {
 
-    const [login, setLogin] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('Logged as:', login);
+        console.log('Registered as:', username);
     }
 
     return (
-        <div className="login">
-            <h1>Zaloguj</h1>
+        <div className="register">
+            <h1>Zarejestruj</h1>
             <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nazwa uzytkownika"
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
                 <input
                     type="text"
                     placeholder="Email"
                     required
-                    value={login}
-                    onChange={(e) => setLogin(e.target.value)}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
                 <input 
                     type="password"
-                    placeholder="Password"
+                    placeholder="Haslo"
                     required
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 ></input>
-                <button>Login</button>
+                <input
+                    type="text"
+                    placeholder="Imie"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Nazwisko"
+                    required
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
+                <button>Zarejestruj</button>
             </form>
         </div>
     );
