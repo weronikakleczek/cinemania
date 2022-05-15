ecb89c5c62b99dad31ce24ac39a4a1e5df50907d
Jakub Frączek <fraczekjakub1999@gmail.com>
2022-05-15 22:56:27 +0200

    updated frontend urls to match apigw

--- cm-frontend/src/components/Top.js
+++ cm-frontend/src/components/Top.js
@@ -1,61 +1,62 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import star from '../assets/icons/star.png';
-
+import star from "../assets/icons/star.png";
 
 const Top = () => {
+  const [movieList, setMovieList] = useState(null);
 
-    const [movieList, setMovieList] = useState(null);
+  useEffect(() => {
+    const abortController = new AbortController();
 
-    useEffect(() => {
+    fetch(`http://localhost:8081/pictures/movie/top`, {
+      signal: abortController.signal,
+      method: "GET",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMovieList(data);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, []);
 
-        const abortController = new AbortController();
-
-        fetch(`http://localhost:8080/movie/top`, {
-            signal: abortController.signal,
-            method: 'GET'
-        })
-        .then(res => {
-                return res.json();
-         })
-        .then(data => {
-            setMovieList(data);
-        })
-        .catch(e => {
-            console.log(e.message);
-        });
-    }, [])
-
-
-    return (
-        <div className="top-container">
-            <h1>Top 100 Filmów</h1>
-            <div className="top-list">
-                { movieList && (
-                        movieList.map((movie, index) => (
-                            <Link to={`/movie/${movie.id}`} className="single-movie" key={ index }>
-                                <h1>{ index + 1 })</h1>
-                                <div className="poster-container">
-                                    <img src={ `https://image.tmdb.org/t/p/original/${movie.poster_path}` } className="poster" alt="movie"/>
-                                </div>
-                                <div className="title-description">
-                                    <h2>{ movie.title}</h2>
-                                    <p>{ movie.overview }</p>
-                                </div>
-                                <div className="score">
-                                    <div className="star-container">
-                                        <img src={ star } alt="movie"/>
-                                    </div>
-                                    <h3>{ movie.vote_average}</h3>
-                                </div>
-                        </Link>
-                        ))
-                    
-                )}
-
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="top-container">
+      <h1>Top 100 Filmów</h1>
+      <div className="top-list">
+        {movieList &&
+          movieList.map((movie, index) => (
+            <Link
+              to={`/movie/${movie.id}`}
+              className="single-movie"
+              key={index}
+            >
+              <h1>{index + 1})</h1>
+              <div className="poster-container">
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                  className="poster"
+                  alt="movie"
+                />
+              </div>
+              <div className="title-description">
+                <h2>{movie.title}</h2>
+                <p>{movie.overview}</p>
+              </div>
+              <div className="score">
+                <div className="star-container">
+                  <img src={star} alt="movie" />
+                </div>
+                <h3>{movie.vote_average}</h3>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Top;
--- cm-frontend/src/components/user/profile/Friends.js
+++ cm-frontend/src/components/user/profile/Friends.js
@@ -1,111 +1,119 @@
 import { useEffect, useState } from "react";
 import ApiCall from "../../../api/ApiCall";
-import remove from "../../../assets/icons/remove.png"
-import profile_pic from "../../../assets/icons/profile_pic.png"
-import {useNavigate} from "react-router-dom";
+import remove from "../../../assets/icons/remove.png";
+import profile_pic from "../../../assets/icons/profile_pic.png";
+import { useNavigate } from "react-router-dom";
 
 const Friends = () => {
+  const [friends, setFriends] = useState([]);
+  const [query, setQuery] = useState("");
+  const [friendsByQuery, setFriendsByQuery] = useState([]);
+  const navigate = useNavigate();
 
-    const [friends, setFriends] = useState([]);
-    const [query, setQuery] = useState('');
-    const [friendsByQuery, setFriendsByQuery] = useState([]);
-    const navigate = useNavigate();
+  useEffect(() => {
+    ApiCall.getAllFriends()
+      .then((res) => {
+        return res.data;
+      })
+      .then((data) => {
+        setFriends(data);
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  }, []);
 
-    useEffect(() => {
-        ApiCall.getAllFriends()
-        .then(res => {
-            return res.data;
-        })
-        .then(data => {
-            setFriends(data);
-        })
-        .catch(err => {
-            console.log('Error: ', err);
-        })
-    }, [])
+  const handleAdd = async (username) => {
+    await ApiCall.addNewFriend(username)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        console.log("Adding new friend, error: ", err.message);
+      });
+    await ApiCall.getAllFriends()
+      .then((res) => {
+        return res.data;
+      })
+      .then((data) => {
+        setFriends(data);
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+    handleFriendsSearchQuery(query);
+  };
 
-    const handleAdd = async (username) => {
-        await ApiCall.addNewFriend(username)
-        .then(res => {
-            return res.data;
+  const handleFriendsSearchQuery = (query) => {
+    setQuery(query);
+    if (query === "") {
+      setFriendsByQuery([]);
+    } else {
+      ApiCall.findUserByQuery(query)
+        .then((res) => {
+          return res.data;
         })
-        .catch(err => {
-            console.log("Adding new friend, error: ", err.message);
-        })
-        await ApiCall.getAllFriends()
-        .then(res => {
-            return res.data;
-        })
-        .then(data => {
-            setFriends(data);
-        })
-        .catch(err => {
-            console.log('Error: ', err);
-        })
-        handleFriendsSearchQuery(query);
+        .then((data) => {
+          setFriendsByQuery(data);
+        });
     }
+  };
 
-    const handleFriendsSearchQuery = (query) => {
-        setQuery(query);
-        if (query === '') {
-            setFriendsByQuery([]);
-        } else {
-            ApiCall.findFriendsByQuery(query)
-            .then(res => {
-                return res.data;
-            })
-            .then(data => {
-                setFriendsByQuery(data);
-            })
-        }
-    }
+  const handleRemoveFriend = (id) => {
+    ApiCall.removeFriendship(id);
+    let newFriendsList = friends.slice().filter((x) => x.userId !== id);
+    setFriends(newFriendsList);
+  };
 
-
-    const handleRemoveFriend = (id) => {
-        ApiCall.removeFriendship(id);
-        let newFriendsList = friends.slice().filter(x => x.userId !== id);
-        setFriends(newFriendsList);
-    }
-
-    return (
-        <div className="single-option friends">
-            <div className="friends-list">
-                <h2>Twoi znajomi:</h2>
-                { friends && (
-                    friends.map((val, idx) => (
-                        <div className="single-friend" key={idx}>
-                            <input type="image" src={ remove } onClick={() => handleRemoveFriend(val.userId)}/>
-                            { val.username }
-                            <input type="image" src={ profile_pic } onClick={() => {navigate(`/user/${val.userId}`)}}/>
-                        </div>
-
-                    ))
-                )}
+  return (
+    <div className="single-option friends">
+      <div className="friends-list">
+        <h2>Twoi znajomi:</h2>
+        {friends &&
+          friends.map((val, idx) => (
+            <div className="single-friend" key={idx}>
+              <input
+                type="image"
+                src={remove}
+                onClick={() => handleRemoveFriend(val.userId)}
+              />
+              {val.username}
+              <input
+                type="image"
+                src={profile_pic}
+                onClick={() => {
+                  navigate(`/user/${val.userId}`);
+                }}
+              />
             </div>
-            <div className="add-friend">
-                <h2>Dodaj znajomego:</h2>
-                <form>
-                    <label>Podaj jego nazwę:</label>
-                    <input 
-                        type="text"
-                        required
-                        value={query}
-                        onChange={e => handleFriendsSearchQuery(e.target.value)}
-                    />
-                </form>
-                { friendsByQuery && (
-                    friendsByQuery.map((val, idx) => (
-                        <div className="queried-friend" key={idx}>
-                            <div className="name">{val.username}</div>
-                            <button className="friend-to-add-button" onClick={() => handleAdd(val.username)}>
-                                Dodaj
-                            </button>
-                        </div>
-                    ))
-                )}
+          ))}
+      </div>
+      <div className="add-friend">
+        <h2>Dodaj znajomego:</h2>
+        <form>
+          <label>Podaj jego nazwę:</label>
+          <input
+            type="text"
+            required
+            value={query}
+            onChange={(e) => handleFriendsSearchQuery(e.target.value)}
+          />
+        </form>
+        {friendsByQuery &&
+          friendsByQuery.map((val, idx) => (
+            <div className="queried-friend" key={idx}>
+              <div className="name">{val.username}</div>
+              <button
+                className="friend-to-add-button"
+                onClick={() => handleAdd(val.username)}
+              >
+                Dodaj
+              </button>
             </div>
-        </div>
-    )
-}
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default Friends
+export default Friends;
