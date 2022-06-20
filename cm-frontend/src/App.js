9dec4f8a164980e500d98b6845aea4f78ae019c1
Jakub Frączek <fraczekjakub1999@gmail.com>
2022-06-20 21:51:46 +0200

    some more polls

--- cm-frontend/src/App.js
+++ cm-frontend/src/App.js
@@ -1,26 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Navbar from './components/Navbar';
-import Home from './components/home/Home';
-import Top from './components/Top';
-import Login from './components/user/Login';
-import Search from './components/search/Search';
-import UserContext from './components/UserContext';
-import './styles/styles.css'
-import Register from './components/user/Register';
-import Filter from './components/filter/Filter';
-import SingleMovie from './components/pictures/SingleMovie';
-import SingleTvShow from './components/pictures/SingleTvShow';
-import Profile from './components/user/profile/Profile';
-import useLocalStorage from './hooks/useLocalStorage';
-import Logout from './components/user/Logout';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/home/Home";
+import Top from "./components/Top";
+import Login from "./components/user/Login";
+import Search from "./components/search/Search";
+import UserContext from "./components/UserContext";
+import "./styles/styles.css";
+import Register from "./components/user/Register";
+import Filter from "./components/filter/Filter";
+import SingleMovie from "./components/pictures/SingleMovie";
+import SingleTvShow from "./components/pictures/SingleTvShow";
+import Profile from "./components/user/profile/Profile";
+import useLocalStorage from "./hooks/useLocalStorage";
+import Logout from "./components/user/Logout";
 import User from "./components/user/User";
 import Ankieta from "./components/Ankieta";
-
-
+import { useState } from "react/cjs/react.development";
 
 const App = () => {
-
   const [user, setUser] = useLocalStorage("user", null);
+  const [list, setList] = useState([]);
 
   return (
     <Router>
@@ -28,8 +27,8 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/search" element={<Search/>} />
-          <Route exact path="/filter" element={<Filter/>} />
+          <Route exact path="/search" element={<Search />} />
+          <Route exact path="/filter" element={<Filter />} />
           <Route exact path="/top" element={<Top />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
@@ -38,11 +37,15 @@ const App = () => {
           <Route exact path="/movie/:id" element={<SingleMovie />} />
           <Route exact path="/tv/:id" element={<SingleTvShow />} />
           <Route exact path="/logout" element={<Logout />} />
-          <Route exact path="/ankieta" element={<Ankieta />} />
+          <Route
+            exact
+            path="/ankieta"
+            element={<Ankieta list={list} setList={setList} />}
+          />
         </Routes>
       </UserContext.Provider>
     </Router>
   );
-}
+};
 
 export default App;
